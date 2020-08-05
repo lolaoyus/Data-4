7d0b7cc7f807b4e525024f9ec237d145a2ce3ff9
lolaoyus <65992615+lolaoyus@users.noreply.github.com>
2020-08-05 00:03:58 -0600

    Lab 4 submission

---
+++ word_embedding.py
@@ -0,0 +1,41 @@
+# %%
+from itertools import combinations
+
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
+text = (
+    "funny comedy music laugh humor song songs jokes musical hilarious"
+)
+doc = nlp(text)
+
+for token1, token2 in combinations(doc, 2):
+    print(
+        f"similarity between {token1} and {token2} is {token1.similarity(token2)}"
+    )
+
+
+# %%
+import pandas as pd
+from gensim.models import Word2Vec
+from tqdm.autonotebook import tqdm
+
+
+data = pd.read_csv("train.csv")
+sentences = []
+for review in tqdm (data["review"]):
+    for sent in doc.sents:
+    doc = nlp(review, disable=["tagger", "ner"]) # tagger  part of speech tag,
+        sentences.append(
+            [t.text for t in sent]
+        )
+
+# train a Word2vec model
+model = Word2Vec(sentences)
+
+
+# %%
+for token1, token2 in combinations(text.split(), 2):
+    print(
+        f"similarity between {token1} and {token2} is {model.wv.similarity(token1, token2)}"
+    )
